fcfda89f4799408a251296cfe8c3a34b0cbecc3e
FeverDreme <jeffreyhuang08@gmail.com>
2020-09-29 15:28:38 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys, os, json
+from docx import Document
+from docx.shared import Inches
+
+from interpreters import interHeader as iH
+from interpreters import interBody as iB
+
+doc = Document()
+
+#Get the selected pynote file
+filearg = sys.argv[1]
+
+with open(filearg) as f:
+    textread = f.read()
+tokens = textread.split()
+
+#JSON Settings
+defaultarg = sys.argv[2]
+
+if defaultarg == 'd': defaultarg = './configs/default.json'
+with open(defaultarg) as jsn:
+    settings = json.load(jsn)
+
+#Read Header
+iH.init(settings)
+
+for ind,word in enumerate(tokens):
+    if iH.headerMode == False and iH.foundHeader == True: break
+    iH.checkForHeader(ind, word)
+
+settings = iH.settings
