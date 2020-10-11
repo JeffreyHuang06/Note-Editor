6310d400816984f554ffe39d3eedebec3a22ca28
FeverDreme <jeffreyhuang08@gmail.com>
2020-10-11 14:35:04 -0400

    -Not finished, to be completed on PC

--- interpreters/interBody.py
+++ interpreters/interBody.py
@@ -1,8 +1,27 @@
 settings = 0
 
+is_h = False
+is_t = False
+is_r = False
+is_l = False
+is_b = False
+is_i = False
+is_emph = False
+
 def init(settingjson : dict):
     global settings
+    global is_h, is_t, is_r, is_l, is_b, is_r, is_i, is_emph
     settings = settingjson
 
+    #create the bool settgins possibly
+    
+
+
 def parseLine(ind : int, line : str):
-    pass
+    tokens = line.split()
+    tokens.append('\n')
+    
+    for tag in tokens:
+        if tag == r'\h':
+            pass
+        
---
+++ utils/switches.py
@@ -0,0 +1,8 @@
+class interBodyTagParser:
+
+    def __init__(self):
+        self.swtichdict = dict()
+
+    
+    def switch(self, tag : str):
+        pass
