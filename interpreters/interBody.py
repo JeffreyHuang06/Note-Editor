1287feb7a976375588c74884d5ffbad4c83c813d
FeverDreme <jeffreyhuang08@gmail.com>
2020-10-13 23:10:42 -0400

    -created mechanism to split lines into tags and text

--- interpreters/interBody.py
+++ interpreters/interBody.py
@@ -21,27 +21,44 @@ def init(settingjson : dict, doc):
     
 
 
-def parseLine(ind : int, line : str):
+def parseLine(lineind : int, line : str):
     #feed the thing char by char in order to make whitespace
     #make a first run to get all the tags, then find them, then add the sentences
     tokens = line.split()
     tokeninds = []
+    lenline = len(line)
     #i could use regex... but i wont
 
     #find all tags
-    tagMode = False
-    lastind = 0
-    for ind, char in enumerate(line):
-        if tagMode:
-            if char == ' ':
-                lastind = ind + 1
-                tagMode = False
-        else:
-            if char == '\\':
-                tagMode = True
-                tokeninds.append((lastind, ind))
-    tokeninds.append((lastind, len(line)))
+    tags = [i for i in tokens if '\\' in i]
+    taginds = []
+    startind = 0
+    for tag in tags:
+        ind = line.find(tag, startind, lenline) # get the inds and lengths of all the tags
+        taginds.append(
+            (ind, ind + len(tag))
+        )
+        startind += ind + len(tag)
 
-    #separate into lists of tags and lines
-    splittedLine = []
+    #make another list of the spearated inds
+    sepinds = []
     lastind = 0
+    for inds in taginds:
+        sepinds.append(
+            (lastind, inds[0])
+        )
+        sepinds.append(
+            (inds[0], inds[1])
+        )
+        lastind = inds[1]
+    sepinds.append(
+        (lastind, lenline-1)
+    )
+
+    #get the actual strings from the inds
+    splines = []
+    for inds in sepinds:
+        splines.append(
+            line[inds[0] : inds[1]]
+        )
+    
